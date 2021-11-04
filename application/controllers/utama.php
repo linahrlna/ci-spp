4366a9b60f3dbf669acc5140469b514d42960a7a
linahrlna <linaherlinaa142@gmail.com>
2021-11-04 09:33:59 +0700

    Create utama.php

---
+++ application/controllers/utama.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class utama extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+        $this->load->view('welcome.spp',$data);
+    }
+
+}
