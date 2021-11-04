60b2708c7f938d3ed2142c17473b2a85ab473ed4
linahrlna <linaherlinaa142@gmail.com>
2021-11-04 14:31:25 +0700

    Create welcome_spp.php

---
+++ application/views/welcome_spp.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Pembayaran SPP</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    
+        <link rel="stylesheet" href="<?php echo media_url() ?>font/iconsmind-s/css/iconsminds.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>font/simple-line-icons/css/simple-line-icons.css" />
+    
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/vendor/bootstrap.min.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/vendor/bootstrap.rtl.only.min.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/vendor/fullcalendar.min.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/vendor/dataTables.bootstrap4.min.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/vendor/datatables.responsive.bootstrap4.min.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/vendor/select2.min.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/vendor/perfect-scrollbar.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/vendor/glide.core.min.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/vendor/bootstrap-stars.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/vendor/nouislider.min.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/vendor/bootstrap-datepicker3.min.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/vendor/component-custom-switch.min.css" />
+        <link rel="stylesheet" href="<?php echo media_url() ?>css/main.css" />
+    
+        <link rel="shortcut icon" href="<?php echo media_url() ?>logo smk.ico" type="image/x-icon">
+        <link rel="icon" href="<?php echo media_url() ?>logo smk.ico" type="image/x-icon">
+    </head>
+    
+    <body id="app-container" class="menu-default show-spinner" <?php echo isset($w) ? $w : null; ?> >
+            
+                <br>
+                <br>
+                <br>
+                <br>
+                    <div class="col-12">
+                        <a class="navbar-logo" href="#">
+                            <span class="logo d-none d-xs-block"></span>
+                            <span class="logo-mobile d-block d-xs-none"></span>
+                        </a>
+                        <br>
+                        <center>
+                        <p class=" black h1">Selamat Datang !</p>
+                        <p class=" green-lime h3">Di web pembayaran SPP </p>
+                        </center>
+                        
+                        
+                    </div>
+                    <br>
+                    <br>
+                   <center>
+                    <div class="col-lg-12 col-xl-50">
+                        <div class="icon-cards-row">
+                            <div class="glide dashboard-numbers">
+                                <div class="glide__track" data-glide-el="track">
+                                    <ul class="glide__slides">
+                                        
+                                        <li class="glide__slide">
+                                            <a href="<?php echo base_url('auth/login'); ?>" class="card">
+                                                <div class="card-body text-center">
+                                                    <i class="iconsminds-student-male-female"></i>
+                                                    <p class="card-text mb-0">MASUK SEBAGAI SISWA</p>
+                                                    
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li class="glide__slide">
+                                            <a href="<?php echo base_url('auth/login'); ?>" class="card">
+                                                <div class="card-body text-center">
+                                                    <i class="iconsminds-consulting"></i>
+                                                    <p class="card-text mb-0">MASUK SEBAGAI PETUGAS</p>
+                                                    
+                                                </div>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </center>
+                
+                            
+    
+        <script src="<?php echo media_url() ?>js/vendor/jquery-3.3.1.min.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/bootstrap.bundle.min.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/Chart.bundle.min.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/chartjs-plugin-datalabels.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/moment.min.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/fullcalendar.min.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/datatables.min.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/perfect-scrollbar.min.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/progressbar.min.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/jquery.barrating.min.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/select2.full.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/nouislider.min.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/bootstrap-datepicker.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/Sortable.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/mousetrap.min.js"></script>
+        <script src="<?php echo media_url() ?>js/vendor/glide.min.js"></script>
+        <script src="<?php echo media_url() ?>js/dore.script.js"></script>
+        <script src="<?php echo media_url() ?>js/scripts.js"></script>
+    
+    </body>
+</html>
