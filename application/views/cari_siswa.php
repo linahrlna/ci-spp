7a97eda12585233b7dce89ca8140caa41de235b4
naufaldezka <naufaldezka@gmail.com>
2021-11-05 09:03:10 +0700

    Create cari_siswa.php

---
+++ application/views/cari_siswa.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Pembayaran SPP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="stylesheet" href="font/iconsmind-s/css/iconsminds.css" />
+    <link rel="stylesheet" href="font/simple-line-icons/css/simple-line-icons.css" />
+
+    <link rel="stylesheet" href="css/vendor/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/vendor/bootstrap.rtl.only.min.css" />
+    <link rel="stylesheet" href="css/vendor/component-custom-switch.min.css" />
+    <link rel="stylesheet" href="css/vendor/glide.core.min.css" />
+    <link rel="stylesheet" href="css/vendor/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="css/vendor/baguetteBox.min.css" />
+    <link rel="stylesheet" href="css/main.css" />
+
+    <link rel="shortcut icon" href="logo smk.ico" type="image/x-icon">
+    <link rel="icon" href="logo smk.ico" type="image/x-icon">
+</head>
+
+<body id="app-container" class="menu-default show-spinner">
+    <nav class="navbar fixed-top">
+        <div class="d-flex align-items-center navbar-left">
+            
+
+            
+
+        </div>
+
+        <a class="navbar-logo" href="Dashboard.Default.html">
+            <span class="logo d-none d-xs-block"></span>
+            <span class="logo-mobile d-block d-xs-none"></span>
+        </a>
+
+        <div class="navbar-right">
+            <div class="header-icons d-inline-block align-middle">
+                <div class="d-none d-md-inline-block align-text-bottom mr-3">
+                    <div class="custom-switch custom-switch-primary-inverse custom-switch-small pl-1" 
+                        data-toggle="tooltip" data-placement="left" title="Dark Mode">
+                        <input class="custom-switch-input" id="switchDark" type="checkbox" checked>
+                        <label class="custom-switch-btn" for="switchDark"></label>
+                    </div>
+                </div>
+
+                <div class="position-relative d-none d-sm-inline-block">
+                    
+                </div>
+
+                <div class="position-relative d-inline-block">
+                    
+                </div>
+
+                <button class="header-icon btn btn-empty d-none d-sm-inline-block" type="button" id="fullScreenButton">
+                    <i class="simple-icon-size-fullscreen"></i>
+                    <i class="simple-icon-size-actual"></i>
+                </button>
+
+            </div>
+
+            <div class="user d-inline-block">
+                <button class="btn btn-empty p-0" type="button" data-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                    <span class="name">Siswa</span>
+                    <span>
+                        <img alt="Profile Picture" src="img/profiles/l-1.jpg" />
+                    </span>
+                </button>
+
+                <div class="dropdown-menu dropdown-menu-right mt-3">
+                    <a class="dropdown-item" href="Pages.Product.images.html">Akun</a>
+                    <a class="dropdown-item" href="utama.html">Keluar</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    
+    
+       <br>
+       </br>
+       <br>
+       </br>
+       <br>
+       </br>
+       <br>
+       </br>
+       <br>
+       </br>
+    
+       <div class="card mb-4">
+        <div class="card-body">
+            <center>
+            <h5 class="mb-4"><b>Cari NISN</b></h5>
+
+            <div class="row">
+                <div class="col-md-12">
+                <div class="col-sm-4">
+                    <div class="input-group typeahead-container">
+                        <input type="text" class="form-control typeahead" name="query" id="query"
+                            placeholder="Cari..." data-provide="typeahead"
+                            autocomplete="off">
+                        <div class="input-group-append ">
+                            <a href="Cek.pembayaran.html">
+                            <button type="submit" class="btn btn-primary default">
+                                <i class="simple-icon-magnifier"></i>
+                            </button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </center>
+        </div>
+    </div>
+                    
+                                 
+    </main>
+    <script src="js/vendor/jquery-3.3.1.min.js"></script>
+    <script src="js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="js/vendor/perfect-scrollbar.min.js"></script>
+    <script src="js/vendor/baguetteBox.min.js"></script>
+    <script src="js/vendor/glide.min.js"></script>
+    <script src="js/dore.script.js"></script>
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
