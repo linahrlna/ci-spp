a5da6c0500d7e0619052e0f22efd0f56cb403826
keyshanabila12 <keyshakeysha813@gmail.com>
2021-11-04 20:57:46 -0700

    Update welcome_spp.php

--- application/views/welcome_spp.php
+++ application/views/welcome_spp.php
@@ -5,23 +5,22 @@
         <title>Pembayaran SPP</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     
-        <link rel="stylesheet" href="<?php echo base_url('assets/font/iconsmind-s/css/iconsminds.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/font/simple-line-icons/css/simple-line-icons.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/dore.light.yellowgranola.min.css');?>" >
-
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/bootstrap.min.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/bootstrap.rtl.only.min.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/fullcalendar.min.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/dataTables.bootstrap4.min.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/datatables.responsive.bootstrap4.min.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/select2.min.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/perfect-scrollbar.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/glide.core.min.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/bootstrap-stars.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/nouislider.min.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/bootstrap-datepicker3.min.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/component-custom-switch.min.css');?>" >
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css');?>" >
+        <link rel="stylesheet" href="<?php echo base_url('assets/font/iconsmind-s/css/iconsminds.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/font/simple-line-icons/css/simple-line-icons.css'); ?>" />
+    
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/bootstrap.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/bootstrap.rtl.only.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/fullcalendar.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/dataTables.bootstrap4.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/datatables.responsive.bootstrap4.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/select2.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/perfect-scrollbar.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/glide.core.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/bootstrap-stars.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/nouislider.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/bootstrap-datepicker3.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/vendor/component-custom-switch.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css'); ?>" />
     
         <link rel="shortcut icon" href="<?php echo base_url('assets/logo-smk.ico'); ?>" type="image/x-icon">
         <link rel="icon" href="<?php echo base_url('assets/logo-smk.ico'); ?>" type="image/x-icon">
