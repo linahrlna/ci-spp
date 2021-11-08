84004ad9a25ade93fd75f400a3399aea2fb63a71
linahrlna <linaherlinaa142@gmail.com>
2021-11-08 15:08:50 +0700

    Create dashboard-ptgs.php

---
+++ application/views/manage/dashboard-ptgs.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Pembayaran SPP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/font/iconsmind-s/css/iconsminds.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/font/simple-line-icons/css/simple-line-icons.css'); ?>" />
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/dore.light.yellowgranola.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/dore.light.yellowgranola.min.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/vendor/bootstrap.min.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/vendor/bootstrap.rtl.only.min.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/vendor/fullcalendar.min.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/vendor/dataTables.bootstrap4.min.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/vendor/datatables.responsive.bootstrap4.min.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/vendor/select2.min.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/vendor/perfect-scrollbar.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/vendor/glide.core.min.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/vendor/bootstrap-stars.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/vendor/nouislider.min.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/vendor/bootstrap-datepicker3.min.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('../assets/css/vendor/component-custom-switch.min.css'); ?>" />
+    <link rel="stylesheet" type="text/css"href="<?php echo base_url('../assets/css/main.css'); ?>" />
+
+    <link rel="shortcut icon" href="<?php echo base_url('assets/logo-smk.ico'); ?>" type="image/x-icon">
+    <link rel="icon" href="<?php echo base_url('assets/logo-smk.ico'); ?>" type="image/x-icon">
+</head>
+
+<body id="app-container" class="menu-default show-spinner">
+    <nav class="navbar fixed-top">
+        <div class="d-flex align-items-center navbar-left">
+            <a href="#" class="menu-button d-none d-md-block">
+                <svg class="main" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 17">
+                    <rect x="0.48" y="0.5" width="7" height="1" />
+                    <rect x="0.48" y="7.5" width="7" height="1" />
+                    <rect x="0.48" y="15.5" width="7" height="1" />
+                </svg>
+                <svg class="sub" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 17">
+                    <rect x="1.56" y="0.5" width="16" height="1" />
+                    <rect x="1.56" y="7.5" width="16" height="1" />
+                    <rect x="1.56" y="15.5" width="16" height="1" />
+                </svg>
+            </a>
+
+            <a href="#" class="menu-button-mobile d-xs-block d-sm-block d-md-none">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 17">
+                    <rect x="0.5" y="0.5" width="25" height="1" />
+                    <rect x="0.5" y="7.5" width="25" height="1" />
+                    <rect x="0.5" y="15.5" width="25" height="1" />
+                </svg>
+            </a>
+
+        </div>
+
+
+        <a class="navbar-logo" href="Dashboard.Default.html">
+            <span class="logo d-none d-xs-block"></span>
+            <span class="logo-mobile d-block d-xs-none"></span>
+        </a>
+
+        <div class="navbar-right">
+            <div class="header-icons d-inline-block align-middle">
+                <div class="d-none d-md-inline-block align-text-bottom mr-3">
+                    <div class="custom-switch custom-switch-primary-inverse custom-switch-small pl-1"
+                         data-toggle="tooltip" data-placement="left" title="Dark Mode">
+                        <input class="custom-switch-input" id="switchDark" type="checkbox" checked>
+                        <label class="custom-switch-btn" for="switchDark"></label>
+                    </div>
+                </div>
+
+                <div class="position-relative d-none d-sm-inline-block">
+                </div>
+                <button class="header-icon btn btn-empty d-none d-sm-inline-block" type="button" id="fullScreenButton">
+                    <i class="simple-icon-size-fullscreen"></i>
+                    <i class="simple-icon-size-actual"></i>
+                </button>
+
+            </div>
+
+            <div class="user d-inline-block">
+                <button class="btn btn-empty p-0" type="button" data-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                    <span class="name">Petugas</span>
+                    <span>
+                        <img alt="Profile Picture" src="img/profiles/l-1.jpg" />
+                    </span>
+                </button>
+
+                <div class="dropdown-menu dropdown-menu-right mt-3">
+                    <a class="dropdown-item" href="Pages.Profil.html">Akun</a>
+                    <a class="dropdown-item" href="index.html">Keluar</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="menu">
+        <div class="main-menu">
+            <div class="scroll">
+                <ul class="list-unstyled">
+                    <li class="active">
+                        <a href="Dashboard.Petugas.html">
+                            <i class="iconsminds-student-hat"></i>
+                            <span>Beranda</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#layouts">
+                            <i class="iconsminds-book"></i> Halaman
+                            
+                        </a>
+                        
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="sub-menu">
+            <div class="scroll">
+                
+                <ul class="list-unstyled" data-link="layouts" id="layouts">
+                    
+                    <li>
+                        <div id="collapseProduct" class="collapse show">
+                            <ul class="list-unstyled inner-level-menu">
+                                <li>
+                                    <a href="Pages.Data.List.html">
+                                        <i class="simple-icon-layers"></i> <span class="d-inline-block">Data Siswa</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="Pages.Transaksi.html">
+                                        <i class="simple-icon-wallet"></i> <span class="d-inline-block">Transaksi</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="Pages.Profil.html">
+                                        <i class="simple-icon-people"></i> <span class="d-inline-block">Profil</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="Pages.Info.html">
+                                        <i class="simple-icon-book-open"></i> <span class="d-inline-block">Info</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    
+
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+                
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Beranda | Sistem Pembayaran SPP</h1>
+                    <nav class="breadcrumb-container d-none d-sm-block d-lg-inline-block" aria-label="breadcrumb">
+                        <ol class="breadcrumb pt-0">
+                        </ol>
+                    </nav>
+                    <div class="separator mb-5"></div>
+                </div>
+                <div class="col-lg-12 col-xl-12">
+                    <div class="icon-cards-row">
+                        <div class="glide dashboard-numbers">
+                            <div class="glide__track" data-glide-el="track">
+                                <ul class="glide__slides">
+                                    <li class="glide__slide">
+                                        <a href="Pages.Data.List.html" class="card">
+                                            <div class="card-body text-center">
+                                                <i class="iconsminds-user"></i>
+                                                <p class="card-text mb-0">Jumlah Siswa</p>
+                                                <p class="lead text-center">64</p>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="glide__slide">
+                                        <a href="Pages.Belum.Lunas.html" class="card">
+                                            <div class="card-body text-center">
+                                                <i class="iconsminds-mail-open"></i>
+                                                <p class="card-text mb-0">Belum Lunas</p>
+                                                <p class="lead text-center">25</p>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="glide__slide">
+                                        <a href="Pages.Lunas.html" class="card">
+                                            <div class="card-body text-center">
+                                                <i class="iconsminds-mail-money"></i>
+                                                <p class="card-text mb-0">Sudah Lunas</p>
+                                                <p class="lead text-center">39</p>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="glide__slide">
+                                        <a href="Pemasukkan.Hari.Ini.html" class="card">
+                                            <div class="card-body text-center">
+                                                <i class="iconsminds-financial"></i>
+                                                <p class="card-text mb-0">Pemasukan Hari Ini</p>
+                                                <p class="lead text-center">Rp.0</p>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 mb-2">
+                    <div class="card h-10">
+                        <div class="card-body">
+                            <h5 class="card-title">Kalender</h5>
+                            <div class="calendar"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-5">
+                    <div class="card h-70">
+                        <a href="Data.Siswa.Bulanan.html" class="card">
+                        <div class="card-body">
+                           <h6 class="mb-4" >Grafik Pembayaran Bulanan</h6>
+                           <div class="chart-container chart" >
+                           <canvas id="areaChart"></canvas>
+                           </div>
+                        </div>
+                        </a>
+                    </div>  
+                </div>
+                <div class="col-lg-6 mb-5">
+                    <div class="card h-70" >
+                        <a href="Data.Siswa.Tahunan.html" class="card">
+                        <div class="card-body">
+                           <h6 class="mb-4" >Grafik Pembayaran Tahunan</h6>
+                           <div class="chart-container chart" >
+                           <canvas id="areaChartNoShadow"></canvas>
+                           </div>
+                        </div>
+                        </a>
+                    </div>  
+                </div>
+            
+            </div>  
+                   
+
+        <script src="<?php echo base_url('../assets/js/vendor/jquery-3.3.1.min.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/bootstrap.bundle.min.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/Chart.bundle.min.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/chartjs-plugin-datalabels.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/moment.min.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/fullcalendar.min.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/datatables.min.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/perfect-scrollbar.min.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/progressbar.min.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/jquery.barrating.min.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/select2.full.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/nouislider.min.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/bootstrap-datepicker.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/Sortable.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/mousetrap.min.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/vendor/glide.min.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/dore.script.js'); ?>"></script>
+        <script src="<?php echo base_url('../assets/js/scripts.js'); ?>"></script>
+    
+</body>
+
+</html>
