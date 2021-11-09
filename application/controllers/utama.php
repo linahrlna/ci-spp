03d591279f942803f391ce1eeb3615ab665edddd
linahrlna <linaherlinaa142@gmail.com>
2021-11-09 15:58:26 +0700

    Update utama.php

--- application/controllers/utama.php
+++ application/controllers/utama.php
@@ -5,9 +5,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Utama extends CI_Controller
 {
 
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct();
+        $this->load->helper('url');
     }
 
     function index()
